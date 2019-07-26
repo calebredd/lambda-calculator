c94bec0607f608d2008b8acaf934a202b5ad958f
Caleb Redd <caleb.redd@yahoo.com>
2019-07-25 17:36:47 -0700

    Imported Numbers from data to build numberButtons

--- src/components/ButtonComponents/NumberButtons/NumberButton.js
+++ src/components/ButtonComponents/NumberButtons/NumberButton.js
@@ -3,17 +3,7 @@ import Numbers from "./Numbers";
 const NumberButton = () => {
   return (
     <div className="numberButtons">
-      <button className="numberBtn btn">1</button>
-      <button className="numberBtn btn">2</button>
-      <button className="numberBtn btn">3</button>
-      <button className="numberBtn btn">4</button>
-      <button className="numberBtn btn">5</button>
-      <button className="numberBtn btn">6</button>
-      <button className="numberBtn btn">7</button>
-      <button className="numberBtn btn">8</button>
-      <button className="numberBtn btn">9</button>
-      <button className="numberBtn btn zero">0</button>
-      <button className="numberBtn btn">.</button>
+      <Numbers/>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import numbers from "../../../data";
 //import any components needed
-
 //Import your array data to from the provided data file
-// const [numberState, setNumberState] = useState(numbers);
 const Numbers = () => {
   // STEP 2 - add the imported data to state
+  const [numberState, setNumberState] = useState(numbers);
+
   return (
     <div>
-      {numbers[0]}
-      {/* {numbers.map((number)=>{
-        return <button className={`${number}`} onClick(()=>{return this.props.number;})>{number}</button>
-      })} */}
+      {numbers.map(number => {
+        if (number != 0) {
+          return <button className="numberBtn btn">{number}</button>;
+        } else {
+          return <button className="numberBtn btn zero">{number}</button>;
+        }
+      })}
 
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
